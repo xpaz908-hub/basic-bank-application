345d5b12777c3d0c1098c570578dd1a48279bb7f
xpaz908 <xpaz908@gmail.com>
2026-02-26 17:31:07 -0500

    small changes

--- users/Customer.java
+++
@@ -1,5 +0,0 @@
-public class Customer {
-    String name;
-    String email;
-    String password;
-}
--- views/LoginView.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class LoginView {
-    Scanner scan = new Scanner(System.in);
-
-    }
-
-
-// System.out.print("\033[H\033[2J"); clear terminal
