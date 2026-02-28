3d42fa5742b24794703cf723585a9547a9b84f3b
xpaz908 <xpaz908@gmail.com>
2026-02-28 01:21:34 -0500

    made instance of welcomeview

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,10 @@
 package main;
+import views.WelcomeView;
+
 public class Main {
     public static void main(String[] args) {
-        
+        WelcomeView welcomeView = new WelcomeView();
+        welcomeView.show();
     }
 }
 
