40587744a2fd12954085496c494a9f7af6caaaaf
xpaz908 <xpaz908@gmail.com>
2026-02-28 02:06:15 -0500

    done with welcome view

--- src/views/WelcomeView.java
+++ src/views/WelcomeView.java
@@ -4,34 +4,34 @@ import util.Util;
 
 public class WelcomeView {
     private Scanner scan = new Scanner(System.in);
+    private boolean running = true;
 
-        public void show() {
-            Util.clearTerminal();
-            System.out.println("Welcome to the Bank!");
-            while(true) {
-                System.out.println("Please select an option:\n");
-                System.out.println("1. Login");
-                System.out.println("2. Register");
-                System.out.println("3. Exit");
-                System.out.print("-> ");
+    public void show() {
+        Util.clearTerminal();
+        System.out.println("Welcome to BankStation!");
+        while(running) {
+            System.out.println("Please select an option:\n");
+            System.out.println("1. Login");
+            System.out.println("2. Register");
+            System.out.println("3. Exit");
+            System.out.print("-> ");
 
-                String input = scan.nextLine();
-                switch (input) {
-                    case "1":
-                        // login view
-                        break;
-                    case "2":
-                        // register view
-                        break;
-                    case "3":
-                        System.out.print("\nGoodbye!");
-                        System.exit(0);
-                        default:
-                        System.out.print("\033[H\033[2J");
-                        System.out.println("Not a valid choice, please try again.\n");
-                }
+            String input = scan.nextLine();
+            switch (input) {
+                case "1":
+                    // login view
+                    running = false;        //?
+                    break;
+                case "2":
+                    // register view
+                    break;
+                case "3":
+                    System.out.print("\nThank you for using BankStation!");
+                    System.exit(0);
+                default:
+                    Util.clearTerminal();
+                    System.out.println("Not a valid choice, please try again.\n");
+            }
         }
     }
 }
-
-// System.out.print("\033[H\033[2J"); clear terminal
