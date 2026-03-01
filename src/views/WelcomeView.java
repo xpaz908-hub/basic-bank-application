867fb9590378b7341f004b10a4851f44e55664ad
xpaz908 <xpaz908@gmail.com>
2026-03-01 01:07:02 -0500

    testing a method for readability

--- src/views/WelcomeView.java
+++ src/views/WelcomeView.java
@@ -1,15 +1,15 @@
 package views;
 import java.util.Scanner;
 import util.*;
-import users.*;
 
-public class WelcomeView {
-    private Scanner scan;
+public class WelcomeView extends View {
     private boolean running = true;
+    private String input;
 
     public WelcomeView(Scanner scan) {
-        this.scan = scan;
+        super(scan);
     }
+    @Override
     public void show() {
         Console.clearTerminal();
         System.out.println("Welcome to BankStation!");
@@ -19,24 +19,28 @@ public class WelcomeView {
             System.out.println("2. Register");
             System.out.println("3. Exit");
             System.out.print("-> ");
+            input = scan.nextLine();
 
-            String input = scan.nextLine();
-            switch (input) {
-                case "1":
-                    // login view
-                    running = false;
-                    break;
-                case "2":
-                    new RegisterView(scan).show();
-                    running = false;
-                    break;
-                    case "3":
-                    System.out.print("\nThank you for using BankStation!");
-                    System.exit(0);
-                default:
-                    Console.clearTerminal();
-                    System.out.println("Not a valid choice, please try again.\n");
-            }
+            chooseOption();
+        }
+    }
+    private void chooseOption() {
+        switch (input) {
+            case "1":
+                // login view
+                running = false;
+                break;
+            case "2":
+                System.out.println("test");
+                new RegisterView(scan).show();
+                running = false;
+                break;
+                case "3":
+                System.out.print("\nThank you for using BankStation!");
+                System.exit(0);
+            default:
+                Console.clearTerminal();
+                System.out.println("Not a valid choice, please try again.\n");
         }
     }
 }
